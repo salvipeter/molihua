09414253f5764b568839a0c72114e878dd1f88b6
Peter Salvi <vukung@yahoo.com>
2025-04-15 12:41:20 +0200

    Simplification

--- run.scm
+++ run.scm
@@ -1,21 +1,13 @@
 (define filename "cube1.obj")
 (define fullness 0.7)
 (define tangent-scale 1.33)
-(define only-one-patch #f)
-(define write-offsets? #t)
-(define write-chamfers? #t)
-(define write-controls? #t)
-(define write-patches? #t)
+(define only-one-patch #f)              ; #f or patch index
 
-(load "molihua.scm")                      ; reload functions (only for debugging)
+(load "molihua.scm")
 (load-model filename)
 
-(when write-offsets?
-  (write-offsets "/tmp/offsets.obj"))
-(when write-chamfers?
-  (write-chamfers "/tmp/chamfers.obj"))
-(when write-controls?
-  (write-controls "/tmp/controls.obj"))
-(when write-patches?
-  (write-patches-mp "/tmp/patch-")
-  (write-patches-cgb "/tmp/patch-"))
+(write-offsets "/tmp/offsets.obj")
+(write-chamfers "/tmp/chamfers.obj")
+(write-controls "/tmp/controls.obj")
+;(write-patches-mp "/tmp/patch-")
+(write-patches-cgb "/tmp/patch-")

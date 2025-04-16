3b1db9658979993aec830a064e01e9ad5bfde698
Peter Salvi <vukung@yahoo.com>
2025-04-16 07:17:33 +0200

    Added test file

---
+++ viewer/build/test.scm
@@ -0,0 +1,7 @@
+(define filename "../../models/cube1.obj")
+(define fullness 0.7)
+(define tangent-scale 1.33)
+(define only-one-patch #f)              ; #f or patch index
+
+(load "../../molihua.scm")
+(load-model filename)
